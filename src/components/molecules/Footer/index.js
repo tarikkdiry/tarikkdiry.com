9cf3a1b6fea11d726978f86640ece98f94d49a13
Tarik Kdiry <tarik.kdiry@gmail.com>
2020-08-08 01:19:51 -0400

    footer

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const Footer = () => {
+    const onMouseOver = e => {
+        const el = e.target;
+        el.style.color = '#FD9374';
+    };
+
+    const onMouseOut = e => {
+        const el = e.target;
+        el.style.color = '#81878C';
+    };
+
+    return (
+        <div className={styles.container}>
+            <a
+                href="https://github.com/tarikkdiry/tarikkdiry.com"
+                target="_blank"
+                onMouseEnter={event => onMouseOver(event)}
+                onMouseOut={event => onMouseOut(event)}
+            >
+                Designed and Developed by Tarik Kdiry
+            </a>
+        </div>
+    )
+}
+
+export default Footer;
