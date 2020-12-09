a125d0cba57b0c5f4e2072749eff31d9e92a2450
tarikkdiry <tarik.kdiry@gmail.com>
2020-12-08 23:58:44 -0500

    randomize intro attribute

--- src/components/molecules/Intro/index.js
+++ src/components/molecules/Intro/index.js
@@ -5,12 +5,19 @@ import resume from "../../../assets/documents/tarik_kdiry_resume.pdf";
 import '../../../styles/_variables.scss';
 import '../../../styles/global.scss';
 
+const randomizeAttribute = () => {
+    let attr = ["fashion enthusiast", "wanna-be stylist", "plant hobbyist"];
+    return attr[Math.floor(Math.random() * attr.length)];
+}
+
 const Intro = () => {
+    let intro = `software engineer. writer. ${randomizeAttribute()}`;
+
     return (
         <div className={styles.container}>
             <p>Hi! I'm</p>
                 <h1>Tarik Kdiry.</h1>
-                <h3>software engineer. writer. wanna-be stylist.</h3>
+                <h3>{`software engineer. writer. ${randomizeAttribute()}`}</h3>
                 <h4>I am a <a href={resume} target="_blank" className={styles.links}>Software Engineer</a> based out of Hoboken, NJ and operating globally from the New York City area.</h4>
                 <h4>Let's connect! While you're here, check out my <Link to='/blog' target="_blank" className={styles.links}>blog!</Link></h4>
         </div>
