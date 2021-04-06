8a176adb1aa1814040416bbab806122b188bf91b
tarikkdiry <tarik.kdiry@gmail.com>
2021-04-05 22:21:24 -0400

    new profile pic

--- src/components/atoms/Profile/index.js
+++ src/components/atoms/Profile/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ProfileImage from "../../../assets/images/tarik_kdiry_profile.png";
+// import ProfileImage from "../../../assets/images/tarik_kdiry_profile.png";
+import ProfileImage from "../../../assets/images/tarik_kdiry_ams.png";
 import styles from "./index.module.scss";
 
 const Profile = () => {
