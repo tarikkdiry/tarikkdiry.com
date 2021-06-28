53c917a1d08f26062221cc81c408f0723233483a
tarikkdiry <tarik.kdiry@gmail.com>
2021-06-27 21:57:53 -0400

    updated about me copy

--- src/components/atoms/Bio/index.js
+++ src/components/atoms/Bio/index.js
@@ -7,16 +7,16 @@ const Bio = () => {
         <div className={styles.container}>
             <h2>about me.</h2>
             <p>
-                As mentioned above, I'm a software engineer. I love writing code :)
-            </p>
-            <p>
-                Currently, I am a software engineer <a href="https://www.leaflink.com/">@LeafLink</a>. When I'm not at the office, I look forward to building new things through my <a href="https://github.com/tarikkdiry" target="_blank">personal projects.</a> 
+                I'm Tarik, and I'm software engineer! Currently I am building cool things <a href="https://www.leaflink.com/">@LeafLink</a>. 
             </p>
             <p>
                 When I'm not behind a screen, I love to travel, watch movies, listen to/play music, learn new languages, etc. 
                 While I do try to pursue new interests, I try to incorporate writing into everything that I do.
             </p>
-            <p>With that, check out my <a href='/blog/' target="_blank">blog</a> where I try to regularly update you on any of my travels, fashion tidbits, coding, and everything in between.</p>
+            <p>With that, check out my <a href='/blog/' target="_blank">blog</a> where I occasionally update you on...my life! Things I'm working on, places I am travelling to, thoughts I am having, etc.</p>
+            <p>
+                Wanna work together? <a href="mailto:tarik.kdiry@gmail.com">Reach out!</a>
+            </p>
         </div>
     )
 }
